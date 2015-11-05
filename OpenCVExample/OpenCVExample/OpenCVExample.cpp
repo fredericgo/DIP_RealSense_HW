9bf32009b0d657fcc2ba55b345aa6a6b1664b9ee
fredericgo <fredericstgo@gmail.com>
2015-11-05 23:59:39 +0800

    new hist

--- OpenCVExample/OpenCVExample/OpenCVExample.cpp
+++ OpenCVExample/OpenCVExample/OpenCVExample.cpp
@@ -106,3 +106,45 @@ int main(int argc, char* argv[]) {
 	return 0;
 
 }
+
+
+void histEq() {
+ /// Establish the number of bins
+	int histSize = 256;
+
+		  /// Set the ranges ( for B,G,R) )
+	float range[] = { 0, 256 } ;
+	const float* histRange = { range };
+
+	bool uniform = true; bool accumulate = false;
+
+	cv::Mat hist;
+
+		  /// Compute the histograms:
+	//calcHist( &bgr_planes[0], 1, 0, cv::Mat(), hist, 1, &histSize, &histRange, uniform, accumulate );
+		  
+
+		  // Draw the histograms for B, G and R
+	int hist_w = 512; int hist_h = 400;
+	int bin_w = cvRound( (double) hist_w/histSize );
+
+	cv::Mat histImage( hist_h, hist_w, CV_8UC3, cv::Scalar( 0,0,0) );
+
+		  /// Normalize the result to [ 0, histImage.rows ]
+	normalize(hist, hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat() );
+		  
+
+		  /// Draw for each channel
+	for( int i = 1; i < histSize; i++ )
+	{
+		line( histImage, cv::Point( bin_w*(i-1), hist_h - cvRound(hist.at<float>(i-1)) ) ,
+						 cv::Point( bin_w*(i), hist_h - cvRound(hist.at<float>(i)) ),
+						 cv::Scalar( 255, 0, 0), 2, 8, 0  );
+	}
+
+		  /// Display
+	cv::namedWindow("calcHist Demo", CV_WINDOW_AUTOSIZE );
+	imshow("calcHist Demo", histImage );
+
+	cv::waitKey(0);
+}
